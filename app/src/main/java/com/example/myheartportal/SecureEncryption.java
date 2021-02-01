d3d8359cc247ee1be93a6abd2f08e514b7fb680b
Abdallah Anugod <abdal98ma@gmail.com>
2021-02-01 17:17:29 +0800

    1-2-21 2:25

--- app/src/main/java/com/example/myheartportal/SecureEncryption.java
+++ app/src/main/java/com/example/myheartportal/SecureEncryption.java
@@ -12,7 +12,6 @@ import javax.crypto.spec.SecretKeySpec;
 public class SecureEncryption
 {
     private final String algorithm = "AES";
-
     public String encryptData(String Data, String password) throws Exception
     {
         SecretKeySpec key = generateKey(password);
@@ -21,7 +20,6 @@ public class SecureEncryption
         byte[] encVal = c.doFinal(Data.getBytes());
         return Base64.encodeToString(encVal, Base64.DEFAULT);
     }
-
     public SecretKeySpec generateKey(String password) throws Exception
     {
         final MessageDigest digest = MessageDigest.getInstance("SHA-256");
@@ -30,7 +28,6 @@ public class SecureEncryption
         byte[] key = digest.digest();
         return new SecretKeySpec(key, "AES");
     }
-
     public String decryptData(String encData, String password) throws Exception
     {
         SecretKeySpec key = generateKey(password);
