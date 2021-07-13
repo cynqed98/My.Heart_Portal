f19ce5f3524bbb47962c42a4eccf822b0810b99b
Abdallah Anugod <abdal98ma@gmail.com>
2021-07-13 11:41:59 +0800

    1-2-21 2:25

--- app/src/main/java/com/example/myheartportal/SecureEncryption.java
+++ app/src/main/java/com/example/myheartportal/SecureEncryption.java
@@ -1,7 +1,10 @@
 package com.example.myheartportal;
 
+import android.annotation.SuppressLint;
 import android.util.Base64;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.security.SecureRandom;
 import java.security.spec.KeySpec;
 
@@ -14,7 +17,11 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class SecureEncryption
 {
-    private final String algorithm = "AES/CBC/PKCS5Padding";
+    //***********************************************************
+    //AES CBC MODE --------------****
+    //Enable or disable by Ctrl + Shift + / on the whole code below until ENDcode
+
+    /*private final String algorithm = "AES/CBC/PKCS5Padding";
     private final int iterationCount = 1000;
     private final int keyLength = 256;
     int saltLength = keyLength / 8;
@@ -61,5 +68,39 @@ public class SecureEncryption
         String plainStr = new String(plaintext, "UTF-8");
 
         return plainStr;
+    }*/
+    //---------------CBC-mode ENDcode****
+
+    //***********************************************************
+
+    //AES EBC MODE (high performance than CBC mode------**
+    //Enable or disable by Ctrl + Shift + / on the whole code below until ENDcode
+
+    private final String algorithm = "AES";
+    public String encryptData(String Data, String password) throws Exception
+    {
+        SecretKeySpec key = generateKey(password);
+        @SuppressLint("GetInstance") Cipher c = Cipher.getInstance(algorithm);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encVal = c.doFinal(Data.getBytes());
+        return Base64.encodeToString(encVal, Base64.DEFAULT);
     }
+    public SecretKeySpec generateKey(String password) throws Exception
+    {
+        final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] bytes = password.getBytes(StandardCharsets.UTF_8);
+        digest.update(bytes, 0, bytes.length);
+        byte[] key = digest.digest();
+        return new SecretKeySpec(key, "AES");
+    }
+    public String decryptData(String encData, String password) throws Exception
+    {
+        SecretKeySpec key = generateKey(password);
+        @SuppressLint("GetInstance") Cipher c = Cipher.getInstance(algorithm);
+        c.init(Cipher.DECRYPT_MODE, key);
+        byte[] decodedValue = Base64.decode(encData, Base64.DEFAULT);
+        byte[] decValue = c.doFinal(decodedValue);
+        return new String(decValue);
+    }
+    //-------------EBC-mode ENDcode****
 }
